66de83c2dc8fa0e24a08d49a40d29714258917a5
marina <marinaaltesmagret@gmail.com>
2021-06-17 21:39:08 +0200

    Adds dotenv config and file including weather API token, succesfully retrieved data from API.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
+import logo from './logo.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      weather: []
+    }
+}
+  componentDidMount() {
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?id=2172797&appid=${process.env.REACT_APP_API_KEY}`)
+      .then(res => this.setState({weather: res.data.weather}));
+  }
+  render() {
+    const { weather } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          {weather.map(weather => <div><h1>Status: {weather.main}</h1><p>Description: { weather.description}</p></div>)}
+          <img src={logo} className="App-logo" alt="logo" />
+          <a
           className="App-link"
           href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        Weather app coming soon...
-        </a>
-      </header>
-    </div>
-  );
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            More details coming soon...
+            </a>
+        </header>
+      </div>
+    );
+  }
 }
+      
 
 export default App;
