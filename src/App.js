1243d3686a96b221876d44a99c6cfbf8d75f6280
marina <marinaaltesmagret@gmail.com>
2021-06-19 21:30:12 +0200

    Adds basic styling (mobile-first) and implements dynamic search bar for current weather in cities

--- src/App.js
+++ src/App.js
@@ -1,36 +1,57 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
-import './App.css';
-import logo from './logo.svg';
-
+import cities from './data/city.list.json';
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      weather: []
+      cities: cities,
+      selectedCities: [],
+      query: '',
+      current: ''
     }
-}
-  componentDidMount() {
-    axios.get(`https://api.openweathermap.org/data/2.5/weather?id=2172797&appid=${process.env.REACT_APP_API_KEY}`)
-      .then(res => this.setState({weather: res.data.weather}));
   }
-  render() {
-    const { weather } = this.state;
+  
+  handleChange = (e) => {
+    if (this.state.current) {
+      this.setState({ current: '' })
+    }
+    const { cities } = this.state;
+    const oldCities = [...cities];
+    let selectedCities = [];
+    oldCities.map(elem => elem.name.toLowerCase().includes(e.target.value.toLowerCase()) ? selectedCities.push(elem) : null);
+    selectedCities = selectedCities.slice(0, 100)
+    this.setState({ query: e.target.value, selectedCities: selectedCities })
+  }
 
+  handleClick = (e) => {
+    const { id } = e.target;
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?id=${id}&appid=${process.env.REACT_APP_API_KEY}`)
+      .then(res => { console.log(res); this.setState({ selectedCities: [], query: '', current: res.data })})
+  }
+
+  render() {
+    const { query, selectedCities, current } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          {weather.map(weather => <div><h1>Status: {weather.main}</h1><p>Description: { weather.description}</p></div>)}
-          <img src={logo} className="App-logo" alt="logo" />
-          <a
-          className="App-link"
-          href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-            >
-            More details coming soon...
-            </a>
-        </header>
+      <div className="app flex flex-col m-auto h-full w-full">
+        <div className="bg-gray-100 bg-opacity-50 rounded-3xl flex flex-col items-center justify-center mt-20 mx-auto w-9/12 h-20 mb-5">
+        <Link to="/"><h1 className="text-2xl text-white">Weather app</h1></Link>
+          </div>
+        <form>
+          <input type="text" onChange={this.handleChange} value={query} className="border block bg-transparent rounded-md m-auto p-3" placeholder="Type your city..."/>
+          <div className="text-center h-10">
+            {selectedCities.map((city, index) => <h1 key={index} id={city.id} onClick={this.handleClick}>{city.name}</h1>)}
+          </div>
+        </form>
+        {current ? <div className="mx-auto">
+          <h1 className="text-2xl">{current.name}</h1>
+          <p>Current weather: {current.weather.description} </p>
+          <p>Temperature: {Math.round(((current.main.temp-273.15)*1.8)+32)} ºF / {Math.round(current.main.temp-273.15)} ºC </p>
+          <p>Humidity: {current.main.humidity} %</p>
+          <p>Wind speed: {Math.round(current.wind.speed)}m/s</p>
+          </div> : ''
+          }
       </div>
     );
   }
